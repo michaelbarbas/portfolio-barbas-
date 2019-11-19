f09a5d67923fd71f3493bd916b60ffacda9052ff
Michael Barbas <michaelbarbas@gmail.com>
2019-11-19 14:20:53 -0500

    testing images

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
-import React from 'react';
-import './App.css';
+import React from "react";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="Name">
-        <h1>Michael Barbas</h1>
+      <header className="Description">
+        <h1 className="Name">
+          Michael Barbas
+          <img src="image/GitHub-Mark-32px" alt=""></img>
+        </h1>
       </header>
     </div>
   );
