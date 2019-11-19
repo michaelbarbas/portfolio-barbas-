f5e4ccca0d49f8e1630bb2cdfc1e331e7e6123c1
Michael Barbas <michaelbarbas@gmail.com>
2019-11-19 16:14:12 -0500

    restore deleted branch | created react app, added header..

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import React from "react";
 import "./App.css";
+import GitHub from "./image/GitHub-Mark-32px.png";
+import LinkedIn from "./image/LinkedIn.png";
 
 function App() {
   return (
@@ -7,7 +9,13 @@ function App() {
       <header className="Description">
         <h1 className="Name">
           Michael Barbas
-          <img src="image/GitHub-Mark-32px" alt=""></img>
+
+    
+          <div class="img">
+            
+            <img src={GitHub} alt="GitHub"></img>
+            <img src={LinkedIn} alt="LinkedIn"></img>
+          </div>
         </h1>
       </header>
     </div>
