be310e11c2c77c2433a6c2150ecaeeacb4d38bca
Michael Barbas <michaelbarbas@gmail.com>
2019-11-19 12:45:36 -0500

    Updates

---
+++ precache-manifest.be7f21f1a845d4c8c305e182e1f95f1f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0fdb7472e5cdec75ff874d2a783f2d45",
+    "url": "/portfolio-barbas/index.html"
+  },
+  {
+    "revision": "c10158cd6e15bd6b91f0",
+    "url": "/portfolio-barbas/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "149c623fe41cfa1806d0",
+    "url": "/portfolio-barbas/static/js/2.57f6af57.chunk.js"
+  },
+  {
+    "revision": "c10158cd6e15bd6b91f0",
+    "url": "/portfolio-barbas/static/js/main.c419e33b.chunk.js"
+  },
+  {
+    "revision": "c55d045da5616a7fc9ee",
+    "url": "/portfolio-barbas/static/js/runtime-main.905a8799.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/portfolio-barbas/static/media/logo.25bf045c.svg"
+  }
+]);
